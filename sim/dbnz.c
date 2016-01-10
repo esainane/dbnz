451db77480b2015f1c2c583c53afc25fdf6f5e57
esainane <esainane+github@gmail.com>
2016-01-10 22:03:10 +1300

    Nicer formatting, more robust parsing

--- sim/dbnz.c
+++ sim/dbnz.c
@@ -9,7 +9,7 @@
 #include <inttypes.h>
 
 static void dbnz_bounds_hcf(const char *what, unsigned int step, size_t cursor, size_t len) {
-  fprintf(stderr, "Invalid access (%s) at %" PRIuMAX  "; upper bound (exclusive) is %" PRIuMAX, what, (uintmax_t) cursor, (uintmax_t) len);
+  fprintf(stderr, "Invalid access (%s) at %" PRIuMAX  "; upper bound (exclusive) is %" PRIuMAX "\n", what, (uintmax_t) cursor, (uintmax_t) len);
   exit(1);
 }
 
@@ -20,17 +20,40 @@ int dbnz_file_bootstrap(const char *filename, void (*stepcallback)(const DBNZ_CE
     fprintf(stderr, "Unable to open file '%s', aborting!\n", filename);
     exit(1);
   }
-  size_t plen, start;
-  fscanf(f, " %z %z ", &plen, &start);
+  /* Terrible workaround for %z not being as widely supported as it should be */
+  uintmax_t tmp1, tmp2;
+  size_t plen = 0, start = 0;
+  /* Read the file header - plen and start. */
+  fscanf(f, " %" PRIuMAX  " %" PRIuMAX  " ", &tmp1, &tmp2);
+  plen = tmp1, start = tmp2;
   DBNZ_CELL_TYPE *state = malloc(sizeof(DBNZ_CELL_TYPE) * plen);
   memset(state, '\0', sizeof(DBNZ_CELL_TYPE) * plen);
   size_t offset;
-  for (offset = 0; offset + 1 < plen; offset += 2) {
-    if (EOF == fscanf(f, " %z %z[^\n]*\n", state + offset, state + offset + 1)) {
-      break;
+  for (offset = 0; offset + 1 < plen; ) {
+    /* Read up to two number from the file. */
+    size_t pos = offset;
+    switch (fscanf(f, "%" PRIuMAX  " %" PRIuMAX, &tmp1, &tmp2)) {
+      case 0: {
+        /* We couldn't parse anything. Try to figure out what went wrong. */
+        char buf[30];
+        fscanf(f, " %29[^\n]", buf);
+        fprintf(stderr, "Parsing error near (%s), is the input file valid?\n", buf);
+        exit(1);
+      }
+      case EOF:
+        return dbnz_bootstrap(state, plen, start, stepcallback);
+      case 2:
+        state[pos + 1] = tmp2;
+        ++offset;
+        /* Fall through */
+      case 1:
+        state[pos    ] = tmp1;
+        ++offset;
     }
+    /* Ignore everything up until the next newline. */
+    fscanf(f, "%*[^\n]*\n");
   }
-  return dbnz_bootstrap(state, plen, start, stepcallback);
+  fprintf(stderr, "File goes beyond memory available to the environment, limit is %" PRIuMAX  ", aborting!\n", (uintmax_t) plen);
 }
 
 int dbnz_bootstrap(DBNZ_CELL_TYPE *state, size_t plen, size_t cursor, void (*stepcallback)(const DBNZ_CELL_TYPE *state, size_t plen, size_t cursor, unsigned int step)) {
@@ -46,10 +69,13 @@ int dbnz_bootstrap(DBNZ_CELL_TYPE *state, size_t plen, size_t cursor, void (*ste
     if (stepcallback) {
       stepcallback(state, plen, cursor, step);
     }
-    size_t target = state[cursor];
+    DBNZ_CELL_TYPE target = state[cursor];
     if (target >= plen) {
       dbnz_bounds_hcf("Decrement target", step, cursor, plen);
     }
+  #ifdef SILLY_VERBOSE
+    printf("Cursor address: %" PRIuMAX  " decrement target address: %" DBNZ_CELL_FMT " decrement target value: %" DBNZ_CELL_FMT " non-zero jump address: %" DBNZ_CELL_FMT "\n", (uintmax_t) cursor, target, state[target], state[cursor + 1]);
+  #endif
     --state[target];
     state[target] &= DBNZ_CELL_MASK;
     /* Branch if non zero, otherwise move to the next line */
